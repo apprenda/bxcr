61f9930c9ba07201c0405e148c7f15065458af6e
Austin Parker <aparker@apprenda.com>
2018-01-03 13:00:05 -0500

    update acp to 8.1 update kismatic to 1.9 remove unneeded chef cookbook support new apprenda windows image w/sqle baked

--- scripts/ps/Download-Apprenda.ps1
+++ scripts/ps/Download-Apprenda.ps1
@@ -1,6 +1,6 @@
 Add-Type -AssemblyName System.IO.Compression.FileSystem
 
-$url = "http://docs.apprenda.com/sites/default/files/Apprenda-8.0.0.zip"
+$url = "http://docs.apprenda.com/sites/default/files/Apprenda-8.1.0.zip"
 $output = "C:\Apprenda.zip"
 if (!(Test-Path -Path C:\Apprenda)) {
   Write-Output "Downloading Apprenda, please wait..."
