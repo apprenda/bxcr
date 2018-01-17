d0c13ab7deab196cb499f56f03e6998650b457d9
Austin Parker <austinlparker@gmail.com>
2018-01-17 15:46:19 -0500

    Update base image and add support for ACP 8.1, KET 1.9 (#13)
    * update to support sql image rather than download
    
    * update acp to 8.1
    update kismatic to 1.9
    remove unneeded chef cookbook
    support new apprenda windows image w/sqle baked
    
    * disable pw expiration on windows

--- chef/cookbooks/apprenda/recipes/disablefirewall.rb
+++ chef/cookbooks/apprenda/recipes/disablefirewall.rb
@@ -1,6 +1,7 @@
 powershell_script "disable windows firewall" do
   code <<-EOH
   Set-NetFirewallProfile -Profile Domain,Public,Private -Enabled False
+  net accounts /maxpwage:unlimited
   exit 0
   EOH
 end
--- scripts/ps/Configure-SQL.ps1
+++ scripts/ps/Configure-SQL.ps1
@@ -1,2 +1,4 @@
 sqlcmd -U sa -P "bxcr@dm!Npaas" -S localhost -Q "create login bxcr with password=N'password', default_database=[master], check_expiration=off, check_policy=off"
 sqlcmd -U sa -P "bxcr@dm!Npaas" -S localhost -Q "exec master..sp_addsrvrolemember @loginame = N'bxcr', @rolename = N'sysadmin'"
+sqlcmd -U sa -P "bxcr@dm!Npaas" -S localhost -Q "EXEC sp_configure 'remote access', 0"
+sqlcmd -U sa -P "bxcr@dm!Npaas" -S localhost -Q "RECONFIGURE"
